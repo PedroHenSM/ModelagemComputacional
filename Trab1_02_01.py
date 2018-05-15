72703d3f39c5205ccceba8340c9293ead8e45f49
Lucas Müller <lucas.mullers@gmail.com>
2018-05-15 14:32:00 -0300

    conclusao da segunda questao

--- Trab1_02_01.py
+++ Trab1_02_01.py
@@ -6,11 +6,31 @@ Created on Mon May 14 14:25:14 2018
 @author: pedrohen
 """
 
-import unumpy as np
 import pylab as pl
 
-ma,mb,me
-
-ma = 0.9*ma + 0.025*me + 0.075*mb
-mb = 0.8*mb + 0.05*me + 0.15*ma
-me = 0.5*me + 0.25*mb + 0.25*ma
+ma = 1000
+mb = 1000
+me = 2000
+man = []
+mbn = []
+men = []
+index = []
+for i in range(30):
+    man.append(0.900*ma + 0.15*mb + 0.25*me)
+    mbn.append(0.075*ma + 0.80*mb + 0.25*me)
+    men.append(0.025*ma + 0.05*mb + 0.50*me)
+    index.append(i)
+    ma = man[i]
+    mb = mbn[i]
+    me = men[i]
+    
+pl.plot(index, man, 'o''-',label="Mercado em alta")
+pl.plot(index, mbn, 'o''-',label="Mercado em baixa")
+pl.plot(index, men, 'o''-',label="Mercado estagnado")
+pl.xlabel("Tempo(semanas)")
+pl.ylabel("Empresas em cada mercado")
+pl.title("Mercados ao longo da semana")
+pl.grid()
+pl.legend(loc='upper right')
+pl.show()
+    
