e47188db0ac1f4e012ef321587b043bf0bf7c932
Lucas Müller <lucas.mullers@gmail.com>
2018-05-14 21:11:36 -0300

    remocao do loop infinito no algoritmo

--- Trab1_01_01.py
+++ Trab1_01_01.py
@@ -3,7 +3,7 @@
 """
 Created on Mon May 14 13:09:15 2018
 
-@author: pedrohen
+@author: pedrohen/lucasmullers
 """
 
 import numpy as np
@@ -12,24 +12,20 @@ import pylab as pl
 def f(r,k):
     return  r + r*k*(1500-r)
 
-#k  = 0.0012
-k =  0.0025
+k  = 0.0012
 r = 4
 
 r_vec = []
 dias_vec = []
 numDias = 0 
 while r < 1500:
-    r_vec.append(f(r,k))
+    r = f(r,k)
+    r_vec.append(r)
     #r = f(r,k)
     dias_vec.append(numDias)
     numDias = numDias + 1
 
-#r_vec.append(r)
-#dias_vec.append(numDias)
-    #r = f(r,k)
 pl.plot(dias_vec,r_vec,'-' 'o',label = "Grafico 1")
-xlim(0,1500)
 pl.grid()
 pl.xlabel("Tempo (dias)")
 pl.ylabel("Boatos (pessoas que sabem)")
