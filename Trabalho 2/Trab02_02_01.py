9e187855eff6c01f64e5942d49177933dae02da9
Lucas Müller <lucas.mullers@gmail.com>
2018-06-29 08:08:04 -0300

    trabalho 2 imagens

--- Trabalho 2/Trab02_02_01.py
+++ Trabalho 2/Trab02_02_01.py
@@ -6,7 +6,6 @@ Created on Sun Jun  3 15:05:37 2018
 @author: lucasmullers
 """
 
-from numpy import random
 import numpy as np
 
 #Cálculo de volume entre dois sólidos usando Monte Carlo
@@ -17,16 +16,19 @@ def solidoB(x,y):
     return np.power(x,2) + 3*np.power(y,2)
 
 
-interno = 0
 iteracoes = 1000000
-for i in range(iteracoes):
-    x = random.rand()*np.sqrt(8.)
-    y = random.rand()*np.sqrt(8./3)
-    z = random.rand()*8.
-
-    if z <= solidoA(x,y) and z <= solidoB(x,y):
-        interno+=1.  
-       
-volume = 4.*8.*np.sqrt(8.)*np.sqrt(8./3)*(interno/iteracoes)
-
-print("Volume = " + str(volume))
+volume = []
+for k in range(100):
+    interno = 0
+    print(k)
+    for i in range(iteracoes):
+        x = np.random.uniform(0, 8)
+        y = np.random.uniform(0, 8)
+        z = np.random.uniform(0, 8)
+    
+        if z <= solidoA(x,y) and z <= solidoB(x,y):
+            interno+=1.
+           
+    volume.append(4.*8.*8.*8.*(interno/iteracoes))
+    
+print("Volume = " + str(sum(volume)/len(volume)))
--- Trabalho 2/testeVolume.py
+++ Trabalho 2/testeVolume.py
@@ -8,7 +8,6 @@ Created on Sun Jun  3 16:15:31 2018
 
 #https://www.tutorbrasil.com.br/forum/viewtopic.php?t=36000
 
-from numpy import random
 import numpy as np
 
 def solidoA(x,y):    
@@ -20,13 +19,13 @@ def solidoB(x,y):
 interno = 0
 iteracoes = 1000000
 for i in range(iteracoes):
-    x = random.uniform(0, np.sqrt(8.))
-    y = random.uniform(0, np.sqrt(8.))
-    z = random.uniform(0, 8.)
+    x = np.random.uniform(0, 8)
+    y = np.random.uniform(0, 8)
+    z = np.random.uniform(0, 8)
 
     if z <= solidoA(x,y) and z <= solidoB(x,y):
         interno+=1.   
        
-volume = 4.*8.*np.sqrt(8.)*np.sqrt(8.)*(interno/iteracoes)
+volume = 4.*8.*8.*8.*(interno/iteracoes)
 
 print("Volume = " + str(volume))
